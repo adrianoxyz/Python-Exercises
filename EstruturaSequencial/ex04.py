0deabea1a6ed22a1142e3c3798f5b24f36601e66
adrianoxyz <adrianoxyz@gmail.com>
2024-11-28 00:02:33 -0300

    Resolução de exercícios em Python

---
+++ EstruturaSequencial/ex04.py
@@ -0,0 +1,9 @@
+# Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+n3 = float(input('Digite a terceira nota: '))
+n4 = float(input('Digite a quarta nota: '))
+
+media = (n1 + n2 + n3 + n4) / 4
+print('A soma da Nota: ', n1, '\ncom a segunda nota: ', n2, '\ncom a terceira nota:', n3, '\ncom a quarta nota:', n4, 'resultou na média: ', media)
---
+++ EstruturaSequencial/ex05.py
@@ -0,0 +1,5 @@
+# Faça um Programa que converta metros para centímetros.
+
+m = float(input('Digite o valor em M: a ser convertido: '))
+cm = m * 100
+print('O valor ', m , 'convertido em cm é = ', cm)
