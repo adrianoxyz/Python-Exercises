1ff8bc03d9c01be9dfb961955bc1ad06760be8a8
adrianoxyz <adrianoxyz@gmail.com>
2025-01-10 16:40:09 -0300

    Resolução de Exercícios

--- EstruturaSequencial/ex07.py
+++ EstruturaSequencial/ex07.py
@@ -1,6 +1,5 @@
 # Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
 
 areaQuadrado = float(input('Digite a área do quadrado: '))
-
 dobroArea = areaQuadrado * areaQuadrado
 print('O dobro da área é: ', dobroArea)
