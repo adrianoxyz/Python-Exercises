ce333ea10cfa614dab4d5bb3bca82a19e4ae939e
adrianoxyz <adrianoxyz@gmail.com>
2025-01-10 16:37:20 -0300

    Resolução de exercícios em Python

---
+++ EstruturaSequencial/ex06.py
@@ -0,0 +1,6 @@
+# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+raioCirculo = float(input('Digite o radio do círculo: '))
+pi = 3.14
+area = (raioCirculo * raioCirculo) * pi
+print('A área é = ', area)
