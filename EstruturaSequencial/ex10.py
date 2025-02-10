233c7a970d4863fa54ce24cb5781016d99c5a457
adrianoxyz <adrianoxyz@gmail.com>
2025-02-10 15:39:53 -0300

    Exercicios em Python

---
+++ EstruturaSequencial/ex10.py
@@ -0,0 +1,5 @@
+# 10 - Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+
+temperaturaC = float(input('Digite a temperatura em Celsius: '))
+temperaturaF = (temperaturaC * 1.8) + 32
+print("A temperatura é Farenheit é: ", temperaturaF)
