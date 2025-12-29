adf95432e21626a7d0f2530f2025e10076aeed20
adrianoxyz <adrianoxyz@gmail.com>
2025-12-29 00:19:24 -0300

    Lista de Exercícios - Python BRasil

---
+++ Estrutura de Decisão/ex01.py
@@ -0,0 +1,11 @@
+# Faça um programa que peça dois números e imprima o maior deles.
+
+a = int(input('Digite o primeiro número a ser comparado: '))
+b = int(input('Digite o segundo número a ser comparado: '))
+
+if a > b:
+    print(f'O maior número é: {a}')
+
+else:
+    print(f'O maior número é: {b}')
+    
